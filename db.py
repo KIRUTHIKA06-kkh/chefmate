d9c6d62419cb89e98aa440a35e955a7796e8e821
KIRUTHIKA06-kkh <kiruthi1811@gmail.com>
2025-01-06 11:46:06 +0000

    db file updated

---
+++ db.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+print('starting')
+try:
+    mydb = mysql.connector.connect(
+       host = 'finaldb.c3i8ggm6665z.ap-south-1.rds.amazonaws.com',
+       port = 3306,
+       user = 'admin',
+       password = 'kkharthik')
+    print("connected")
+    
+except:
+    print('not connected')
+
+print('end')
