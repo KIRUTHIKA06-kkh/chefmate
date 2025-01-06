70c520a3f6a5ee5679781fee8244049f6b1b98d1
KIRUTHIKA06-kkh <kiruthi1811@gmail.com>
2025-01-06 11:47:57 +0000

    password removed

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ try:
        host = 'finaldb.c3i8ggm6665z.ap-south-1.rds.amazonaws.com',
        port = 3306,
        user = 'admin',
-       password = 'kkharthik')
+       password = '')
     print("connected")
     
 except:
