717c9914a302302447e1e8b81cb17b5e1b36c60e
KIRUTHIKA06-kkh <kiruthi1811@gmail.com>
2025-01-06 12:36:47 +0000

    saved

--- db.py
+++ db.py
@@ -1,15 +1,19 @@
 import mysql.connector
-
+from dotenv import load_dotenv
+import os
 print('starting')
+load_dotenv()
 try:
     mydb = mysql.connector.connect(
        host = 'finaldb.c3i8ggm6665z.ap-south-1.rds.amazonaws.com',
        port = 3306,
        user = 'admin',
-       password = '')
+       password = os.getenv('dbpassword'))
+
     print("connected")
     
 except:
     print('not connected')
 
 print('end')
+
