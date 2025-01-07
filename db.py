c3131d9ee221625e83b6e39cf4fa2f31a94c365c
KIRUTHIKA06-kkh <kiruthi1811@gmail.com>
2025-01-07 07:23:50 +0000

    saved

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ print('starting')
 load_dotenv()
 try:
     mydb = mysql.connector.connect(
-       host = 'finaldb.c3i8ggm6665z.ap-south-1.rds.amazonaws.com',
+       host = 'chefmatedb.c3i8ggm6665z.ap-south-1.rds.amazonaws.com',
        port = 3306,
        user = 'admin',
        password = os.getenv('dbpassword'))
